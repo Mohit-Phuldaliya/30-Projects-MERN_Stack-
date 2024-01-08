b9bf5014ec70326955fe121077bdf4911906a4b8
Mohit-Phuldaliya <phuldaliyamohit@gmail.com>
2024-01-08 06:26:41 +0530

    Setting Views(EJS) files URL Short

--- URL Short(Nodejs, Expressjs, MongoDB)/controllers/url.js
+++ URL Short(Nodejs, Expressjs, MongoDB)/controllers/url.js
@@ -4,15 +4,17 @@ const URL = require("../models/url");
 async function handleGenerateNewShortURL(req, res) {
   const body = req.body;
   if (!body.url) return res.status(400).json({ error: "url is required" });
-  const shortID = shortid();
+
+  const shortID = shortid.generate();
 
   await URL.create({
     shortId: shortID,
     redirectURL: body.url,
     visitHistory: [],
   });
-
-  return res.json({ id: shortID });
+  return res.render("home", {
+    id: shortID,
+  });
 }
 
 async function handleGetAnalytics(req, res) {
--- URL Short(Nodejs, Expressjs, MongoDB)/index.js
+++ URL Short(Nodejs, Expressjs, MongoDB)/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { connectToMongoDB } = require("./connect");
 const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter");
 const URL = require("./models/url");
+const path = require("path");
 
 const app = express();
 const PORT = 8000;
@@ -10,9 +12,14 @@ connectToMongoDB("mongodb://localhost:27017/short-url2").then(() =>
   console.log("Mongodb connected")
 );
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/url", urlRoute);
+app.use("/", staticRoute);
 
 app.get("/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
@@ -28,7 +35,11 @@ app.get("/:shortId", async (req, res) => {
       },
     }
   );
-  res.redirect(entry.redirectURL);
+  if (entry && entry.redirectURL) {
+    res.redirect(entry.redirectURL);
+  } else {
+    res.status(404).send("URL not found");
+  }
 });
 
 app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
